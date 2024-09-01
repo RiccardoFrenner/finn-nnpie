Fix c error computation

src/common.py
@@ -380,7 +380,7 @@ class ConcentrationPredictor(nn.Module):
     def run_training(
         self,
         t: torch.Tensor,
-        u_full_train: torch.Tensor,
+        u_train: torch.Tensor,
         max_epochs: int = 100,
         c_field_seed=None,
     ):
@@ -418,18 +418,19 @@ class ConcentrationPredictor(nn.Module):
         np.save(out_dir / "retardation_linear.npy", ret_linear)
         np.save(out_dir / "retardation_freundlich.npy", ret_freundlich)
         np.save(out_dir / "retardation_langmuir.npy", ret_langmuir)
-        np.save(out_dir / "c_train.npy", u_full_train.numpy())
+        np.save(out_dir / "c_train.npy", u_train.numpy())
         np.save(out_dir / "t_train.npy", t.numpy())
 
         if c_field_seed is not None:
             rng = np.random.default_rng(c_field_seed)
-            field_mask = rng.uniform(size=u_full_train.shape) < 0.5
+            field_mask = rng.uniform(size=u_train.shape) < 0.5
 
         # Define the closure function that consists of resetting the
         # gradient buffer, loss function calculation, and backpropagation
         # The closure function is necessary for LBFGS optimizer, because
         # it requires multiple function evaluations
         # The closure function returns the loss value
+        epoch = 0
         def closure():
             self.train()
             optimizer.zero_grad()
@@ -437,13 +438,13 @@ class ConcentrationPredictor(nn.Module):
             # TODO: mean instead of sum?
             if c_field_seed is not None:
                 # set the not used training data to be equal to the prediction. So no error is calculated there
-                # u_full_train[field_mask] = ode_pred[field_mask]
+                # u_train[field_mask] = ode_pred[field_mask]
 
                 loss = self.cfg.error_mult * torch.sum(
-                    (u_full_train[field_mask] - ode_pred[field_mask]) ** 2
+                    (u_train[field_mask] - ode_pred[field_mask]) ** 2
                 )
             else:
-                loss = self.cfg.error_mult * torch.sum((u_full_train - ode_pred) ** 2)
+                loss = self.cfg.error_mult * torch.sum((u_train - ode_pred) ** 2)
 
             # Physical regularization: value of the retardation factor should decrease with increasing concentration
             ret_inv_pred = self.retardation_inv_scaled(u_ret)
@@ -453,10 +454,14 @@ class ConcentrationPredictor(nn.Module):
 
             loss.backward()
 
+            c_pred_path = out_dir / f"predicted_concentrations/c_pred_{epoch}.npy"
+            np.save(c_pred_path, ode_pred.detach().numpy())
+
             return loss
 
         # Iterate until maximum epoch number is reached
-        for epoch in range(1, max_epochs + 1):
+        for _ in range(1, max_epochs + 1):
+            epoch += 1
             dt = time.time()
             optimizer.step(closure)
             loss = closure()

src/make_c_PI_datasets.py
@@ -22,10 +22,10 @@ def make_dataset(mode, quantile, ret_type):
     #     f"Min/Max {mode} residual tot: {full_residuals_tot.min()} {full_residuals_tot.max()}"
     # )
 
-    Y_finn_mean_diss = np.load(base_dir / "c_predictions.npy")[:, 0, ...].reshape(
+    Y_finn_mean_diss = np.load(base_dir / "c_train_predictions.npy")[:, 0, ...].reshape(
         (-1, 1)
     )
-    Y_finn_mean_tot = np.load(base_dir / "c_predictions.npy")[:, 1, ...].reshape(
+    Y_finn_mean_tot = np.load(base_dir / "c_train_predictions.npy")[:, 1, ...].reshape(
         (-1, 1)
     )
     residual_medians = np.load(base_dir / "residual_medians.npy")

src/make_c_residual_data.py
@@ -19,7 +19,7 @@ image_out_dir.mkdir(parents=True, exist_ok=True)
 
 # %%
 c_train = np.load(base_dir / "c_train.npy")
-c_predictions = np.load(base_dir / "c_predictions.npy")
+c_predictions = np.load(base_dir / "c_train_predictions.npy")
 residuals = c_train - c_predictions
 
 NOISE_SIGMA = 0  # 8e-4  # 4e-4 is the lowest I can go since otherwise there will be too large gaps

src/train_finn.py
@@ -42,13 +42,16 @@ def main(
     print(f"{t_train.shape=}")
 
     Y = torch.from_numpy(np.load(y_train_path)).float().unsqueeze(-1)
+    Y_train = Y[:train_split_idx].clone()
+
     num_vars = 2
     assert Y.shape == (
-        len(t_train),
+        len(t),
         num_vars,
         cfg.Nx,
         1,
     ), f"{Y.shape} != {(len(t_train), num_vars, cfg.Nx, 1)}"
+    assert Y.shape[0] == train_split_idx
 
     cfg.model_path = output_dir.resolve()
     clear_dirs = False
@@ -58,7 +61,7 @@ def main(
         raise ValueError(f"Folder {cfg.model_path} already exists.")
     cfg.model_path.mkdir(parents=True, exist_ok=True)
 
-    u0 = Y[0].clone()
+    u0 = Y_train[0].clone()
     model = ConcentrationPredictor(
         u0=u0,
         cfg=cfg,
@@ -70,9 +73,16 @@ def main(
 
     # Train the model
     model.run_training(
-        t=t_train, u_full_train=Y, max_epochs=max_epochs, c_field_seed=c_field_seed
+        t=t_train, u_train=Y_train, max_epochs=max_epochs, c_field_seed=c_field_seed
     )
-    return model, t_train, Y
+
+    model.eval()
+    with torch.no_grad():
+        c_predictions = model(t)
+    np.save(args["output_dir"] / "c_full.npy", Y.detach().numpy())
+    np.save(args["output_dir"] / "c_full_predictions.npy", c_predictions.detach().numpy())
+
+    return model, t_train, Y_train
 
 
 if __name__ == "__main__":
@@ -122,4 +132,4 @@ if __name__ == "__main__":
     model.eval()
     with torch.no_grad():
         c_predictions = model(t_train)
-    np.save(args["output_dir"] / "c_predictions.npy", c_predictions)
+    np.save(args["output_dir"] / "c_train_predictions.npy", c_predictions.detach().numpy())